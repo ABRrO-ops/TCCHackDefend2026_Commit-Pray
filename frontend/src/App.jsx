449d45a6366a76c180c4bef0a17d80186efa45ca
ABRO <abdoulmadjidbawa3@gmail.com>
2026-06-03 00:32:00 +0000

    feat: Mise en plce du setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Dashboard Admin 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
