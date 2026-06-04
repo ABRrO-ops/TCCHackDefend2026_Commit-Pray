a19a221535071c04911704fc460d3205be7e28da
ABRrO-ops <abdoulmadjidbawa6@gmail.com>
2026-06-04 10:56:37 +0000

    recréation des fichiers pour la navigation entre les pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import DashboardMembre from "./pages/DashboardMembre"
+import DashboardCollecteur from "./pages/DashboardCollecteur"
+import DashboardAdmin from "./pages/DashboardAdmin"
+
 function App() {
   return (
-    <div className="p-6">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Dashboard Admin 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/membre" element={<DashboardMembre />} />
+        <Route path="/collecteur" element={<DashboardCollecteur />} />
+        <Route path="/admin" element={<DashboardAdmin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
