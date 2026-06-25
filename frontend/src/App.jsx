37a3de4cb9d6cf5a2eecae33fb87dadb4064e7ab
ABRrO-ops <abdoulmadjidbawa6@gmail.com>
2026-06-25 13:33:09 +0000

    Branchement de la route dans App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
+import Inscription from "./pages/Inscription"
 import DashboardMembre from "./pages/DashboardMembre"
 import DashboardCollecteur from "./pages/DashboardCollecteur"
 import DashboardAdmin from "./pages/DashboardAdmin"
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/inscription" element={<Inscription />} />
         <Route path="/membre" element={<DashboardMembre />} />
         <Route path="/membre/retrait" element={<DemandeRetrait />} />
         <Route path="/collecteur" element={<DashboardCollecteur />} />
